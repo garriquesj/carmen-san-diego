01803543f41290114390035434fd152d22b28839
Jason <jay.garriques@gmail.com>
2022-02-04 20:20:13 -0500

    completed

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -11,39 +11,75 @@ SELECT name, code, population FROM country WHERE region = 'Southern Europe' ORDE
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
-
+--table is country language attribute is countrycode which is VAT
+SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
+world=# SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ VAT         | Italian  | t          |          0
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+ SELECT c.region, c.name, c.code
+FROM countrylanguage cl, country c
+WHERE language = 'Italian'
+AND cl.countrycode = c.code
+AND c.region = 'Southern Europe'
+AND 1 = (SELECT COUNT(language)
+FROM countrylanguage WHERE countrycode = c.code);
+     region      |             name              | code 
+-----------------+-------------------------------+------
+ Southern Europe | San Marino                    | SMR
+ Southern Europe | Holy See (Vatican City State) | VAT
+(2 rows) 
+ there was no chance of me figuring this one out lol 
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
-
-
+so search cities  but not the one that is the same as the countryies name sthe countries cities???
+SELECT * FROM city WHERE countrycode = 'SMR' AND name != 'San Marino';
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+-- so Need a search for similar command
+SELECT * FROM city WHERE name LIKE 'Serr%';
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+  265 | Serra      | BRA         | Esp�rito Santo    |     302666
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+(2 rows)
+SELECT * FROM city WHERE name LIKE 'Serr%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+capital of brazil
+ Select capital FROM country WHERE name  = 'Brazil';
+ capital 
+---------
+     211
+(1 row)
 
-
+world=# 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
-
-
+match city or country to number?
+SELECT * FROM city WHERE id = '211';
+ id  |   name   | countrycode |     district     | population 
+-----+----------+-------------+------------------+------------
+ 211 | Bras�lia | BRA         | Distrito Federal |    1969868
+(1 row)
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
@@ -53,11 +89,15 @@ SELECT name, code, population FROM country WHERE region = 'Southern Europe' ORDE
 -- And while the food here is great, and the people – so nice!
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
--- In a city of ninety-one thousand and now, eighty five.
+-- In a city of ninety-one thousand and now, eighty five.91084
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+FROM city WHERE population = 91084;
+  id  |     name     | countrycode |  district  | population 
+------+--------------+-------------+------------+------------
+ 4060 | Santa Monica | USA         | California |      91084
+(1 row)
 
-
--- She's in ______________________!
+-- She's in _________Santa Monica_____________!
